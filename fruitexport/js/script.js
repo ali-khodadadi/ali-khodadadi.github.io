65ba9e943ccf5aca76fb12cbe8b8ef3f16baba9b
ali-khodadadi <69278014+ali-khodadadi@users.noreply.github.com>
2020-09-24 23:11:41 +0330

    Add files via upload

---
+++ fruitexport/js/script.js
@@ -0,0 +1,36 @@
+$(function() {
+  $('#adv-card1').hover(function() {
+    $('#icon-adv-1').css('color', '#00ACC1');
+    $('#adv-card1').addClass('shadow-lg');
+
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#icon-adv-1').css('color', '#26C6DA');
+    $('#adv-card1').removeClass('shadow-lg');
+  });
+});
+
+$(function() {
+  $('#adv-card2').hover(function() {
+    $('#icon-adv-2').css('color', '#00ACC1');
+    $('#adv-card2').addClass('shadow-lg');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#icon-adv-2').css('color', '#26C6DA');
+    $('#adv-card2').removeClass('shadow-lg');
+
+  });
+});
+
+
+$(function() {
+  $('#adv-card3').hover(function() {
+    $('#icon-adv-3').css('color', '#00ACC1');
+    $('#adv-card3').addClass('shadow-lg');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#icon-adv-3').css('color', '#26C6DA');
+    $('#adv-card3').removeClass('shadow-lg');
+
+  });
+});
